2b485ba1ab459b36e005e5d1eacf51962c3c7787
Pratham082004 <prathampatil.sit.comp@gmail.com>
2026-07-01 13:08:58 +0530

    Added database connection

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,6 @@ from flask_cors import CORS
 from config import Config
 from database.connection import db
 
-from routes.health_routes import health_bp
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -13,7 +11,6 @@ CORS(app)
 
 db.init_app(app)
 
-app.register_blueprint(health_bp)
 
 if __name__ == "__main__":
     with app.app_context():
