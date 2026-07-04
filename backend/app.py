7607502a5f24be32d2de8f3c2bc1986b5ba3ee1b
Pratham082004 <prathampatil.sit.comp@gmail.com>
2026-07-04 23:26:17 +0530

    updated app file

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 from config import Config
 from database.connection import db
 from utils.responses import success_response
+from utils.logger import logger
+
 
 from routes.health_routes import health_bp
 from routes.package_routes import package_bp
@@ -36,20 +38,22 @@ def create_app():
 
 app = create_app()
 
-
 if __name__ == "__main__":
 
     with app.app_context():
         try:
             db.engine.connect()
-            print("Database connection successful.")
+            logger.info("Database connection successful.")
+
         except Exception as e:
-            print(f"Database connection failed: {e}")
+            logger.error(f"Database connection failed: {e}", exc_info=True)
 
     app.run(
         host=Config.HOST,
         port=Config.PORT,
-        debug=True,  
+        debug=True,
+        use_reloader=False,
     )
+
 
     
