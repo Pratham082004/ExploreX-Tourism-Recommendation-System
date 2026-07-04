d1fde599b326ed1ec2017266d1a12f7eb7da6df3
Pratham082004 <prathampatil.sit.comp@gmail.com>
2026-07-04 15:04:25 +0530

    Updated the all the repositories

--- backend/repositories/package_repository.py
+++ backend/repositories/package_repository.py
@@ -7,52 +7,41 @@ SQLAlchemy models.
 
 from database.connection import db
 from database.models import Domestic_Package, International_Package
+from utils.logger import logger
 
 
 class PackageRepository:
-    """Data access layer for travel packages."""
+
+    """ Data access layer for travel packages.
+        Data is extracted from the database.  
+    """
     @staticmethod
     def get_all_domestic_packages():
         """Retrieves all domestic packages."""
+        logger.debug("Repository: get_all_domestic_packages")
         return Domestic_Package.query.all()
+
 
     @staticmethod
     def get_domestic_package_by_id(package_id):
         """Retrieves a domestic packages by ID."""
-        return Domestic_Package.query.filter_by(
-            package_id=package_id
-            ).first()
+        logger.debug("Repository: get_domestic_package_by_id id=%s", package_id)
+        return Domestic_Package.query.filter_by(package_id=package_id).first()
 
-    @staticmethod
-    def get_domestic_package_by_duration(duration):
-        """Retrieves a domestic packages by their duration."""
-        return Domestic_Package.query.filter_by(
-            duration=duration
-            ).all()
-    
 
     @staticmethod
     def get_all_international_packages():
         """Retrieves all international package."""
+        logger.debug("Repository: get_all_international_packages")
         return International_Package.query.all()
+
     
+    @staticmethod
     def get_international_package_by_id(package_id):
         """Retrieves an international package by its ID."""
-        return International_Package.query.filter_by(
-            package_id=package_id
-            ).first()
-    
-    def get_international_package_by_duration(duration):
-        """Retrieves an international packages filtered by duration."""
-        return International_Package.query.filter_by(
-            duration=duration
-            ).all()
-    
-    def get_international_package_by_country(country):
-        """Retrieves an international packages filtered by country."""
-        return International_Package.query.filter_by(
-            country=country
-            ).all()
+        logger.debug("Repository: get_international_package_by_id id=%s", package_id)
+        return International_Package.query.filter_by(package_id=package_id).first()
 
+    
 
 
