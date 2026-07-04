50bee361c6f181f4e61e4b96b3d73ae8cd9f31b7
Pratham082004 <prathampatil.sit.comp@gmail.com>
2026-07-05 01:30:15 +0530

    Updated frontend entry point and application structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 import Home from "./page/Home";
 import Recommendation from "./page/Recommendation";
 import Package from "./page/Package";
 import About from "./page/About";
+import Contact from "./page/Contact";
 import NotFound from "./page/NotFound";
+import Recommendations from "./page/Recommendations";
 
 function App(){
   return(
     <>
         <Navbar />
-
+        
+          <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12 flex flex-col">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/recommendation" element={<Recommendation />} />
-              <Route path="/package/:id" element={<Package />} />
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/recommendation" element={<Recommendation />} />
+
+                <Route path="/package/domestic/:id" element={<Package />} />
+                <Route path="/package/international/:id" element={<Package />} />
+
+                <Route path="/recommendations" element={<Recommendations />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
+          </main>
 
         <Footer />
     </>
@@ -28,4 +37,3 @@ function App(){
 }
 
 export default App;
-
